d223abd03611a1d9a5b69396be5829f2bda2ab23
AzatSaralaev1 <150483814+AzatSaralaev1@users.noreply.github.com>
2023-11-18 12:11:23 +0600

    Add files via upload
    ДЗ-2

--- myproject_program_for_students/js/home_works.js
+++ myproject_program_for_students/js/home_works.js
@@ -34,3 +34,46 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     moveBlock();
 });
+
+
+// Homework 2
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+const minutesDisplay = document.getElementById('minutesS');
+const secondsDisplay = document.getElementById('secondsS');
+const millisecondsDisplay = document.getElementById('ml-secondsS');
+let timerInterval, minutes = 0, seconds = 0, milliseconds = 0;
+function updateDisplay() {
+    minutesDisplay.textContent = minutes < 10 ? '0' + minutes : minutes;
+    secondsDisplay.textContent = seconds < 10 ? '0' + seconds : seconds;
+    millisecondsDisplay.textContent = milliseconds < 10 ? '0' + milliseconds : milliseconds;
+}
+startButton.addEventListener('click', () => {
+    if (!timerInterval) {
+        timerInterval = setInterval(() => {
+            milliseconds++;
+            if (milliseconds === 100) {
+                milliseconds = 0;
+                seconds++;
+                if (seconds === 60) {
+                    seconds = 0;
+                    minutes++;
+                }
+            }
+            updateDisplay();
+        }, 10);
+    }
+});
+stopButton.addEventListener('click', () => {
+    clearInterval(timerInterval);
+    timerInterval = undefined;
+});
+resetButton.addEventListener('click', () => {
+    clearInterval(timerInterval);
+    timerInterval = undefined;
+    minutes = seconds = milliseconds = 0;
+    updateDisplay();
+});
+updateDisplay();
+
